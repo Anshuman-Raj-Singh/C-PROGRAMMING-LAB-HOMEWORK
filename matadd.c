7026a468b651fb30b95f5241712dab409fb3e34f
ANSHUMAN RAJ SINGH <146862435+Anshuman-Raj-Singh@users.noreply.github.com>
2024-01-10 11:54:42 +0530

    matadd.c

--- matadd.c
+++ matadd.c
@@ -6,6 +6,5 @@ void main(){
     printf("enter the limits:");
     scanf("%d%d",&m,&n);
     int a[m][n],b[m][n],c[m][n],i,j;
-    printf("A matrix elements:");
-    
+    printf("A matrix elements:");  
 }
